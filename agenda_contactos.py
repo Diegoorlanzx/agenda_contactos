f723b832bfc1752101ca6771dd2d636f37488af1
Diegoorlanzx <155659029+Diegoorlanzx@users.noreply.github.com>
2025-06-12 18:47:32 -0500

    feat: agregar función para editar contactos

--- agenda_contactos.py
+++ agenda_contactos.py
@@ -12,7 +12,7 @@ agenda= {
         'direccion': 'calle secundaria'
     },
 
-    'pedor': {
+    'pedro': {
         'telefono': '55547541256',
         'email': 'pedor@gmail.com',
         'direccion': 'calle tercera'
@@ -20,3 +20,42 @@ agenda= {
 }
 
 print(agenda)
+
+print(f''' Informacion del contacto maria :
+    Telefono: {agenda ['maria']['telefono']}
+    Email: {agenda ['maria']['email']}
+    Direccion: {agenda ['maria'] ['direccion']}
+    
+''')
+
+agenda['ana']= {
+        'telefono': '555878546369',
+        'email': 'ana@gmail.com',
+        'direccion': 'calle cuarta'
+}
+
+print(agenda)
+
+agenda.pop('pedro')
+print(agenda)
+
+for nombre, detalles in agenda.items():
+    print('  ')
+    print(f'''nombre: {nombre}
+        'Telefono': {detalles.get('telefono')},
+        'Email': {detalles.get('email')},
+        'Direccion': {detalles.get('direccion')}'
+''')
+
+personas= [{'nombre' : 'regina', 'apellido' : 'florez', 'edad' : '20'},
+           {'nombre' : 'alejandro', 'apellido' : 'reyes', 'edad' : '30'}]
+
+print(f'''
+    'nombre': {personas[0].get('nombre')}
+    'apellido': {personas[0].get('apellido')}
+    'edad':{personas[0].get('edad')}        
+''')
+
+for contador,persona in enumerate(personas):
+    print(f'{contador} - persona:{persona}')
+    print(f'detalle: nombre:{persona.get('nombre')}, apellido:{persona.get('apellido')}')
