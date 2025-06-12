8fb8fc72d5692abe6d89786c5a5fbe6117164a7b
Diegoorlanzx <155659029+Diegoorlanzx@users.noreply.github.com>
2025-06-12 17:11:45 -0500

    feat: subir proyecto agenda_contactos en Python

---
+++ agenda_contactos.py
@@ -0,0 +1,22 @@
+print('*** Agenda Contactos ***')
+
+agenda= {
+    'carlos': {
+        'telefono': '55584789',
+        'email': 'carlos@gmail.com',
+        'direccion': 'calle principal'
+    },
+    'maria': {
+        'telefono': '55585555',
+        'email': 'maria@gmail.com',
+        'direccion': 'calle secundaria'
+    },
+
+    'pedor': {
+        'telefono': '55547541256',
+        'email': 'pedor@gmail.com',
+        'direccion': 'calle tercera'
+    }
+}
+
+print(agenda)
